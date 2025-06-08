66ef3fae968af93dfa7439a2f4b10e1ed6c41106
Angelina <rebeka.gilberd@mail.ru>
2025-06-08 17:32:56 +0300

    first commit7

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -2,44 +2,49 @@ package ru.netology.data;
 
 import lombok.Value;
 
+import java.util.Random;
+
 public class DataHelper {
     private DataHelper() {
     }
 
     @Value
     public static class AuthInfo {
-        private String login;
-        private String password;
-    }
-
-    public static AuthInfo getAuthInfo() {
-
-        return new AuthInfo("vasya", "qwerty123");
+        String login;
+        String password;
     }
 
     @Value
     public static class VerificationCode {
-        private String code;
-    }
-
-    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
-
-        return new VerificationCode("12345");
+        String code;
     }
 
     @Value
-    public static class CardInfo {
-        private String cardNumber;
+    public static class CardValue {
+        String cardNumber;
     }
 
-    public static CardInfo getFirstCardInfo () {
-
-        return new CardInfo("5559000000000001");
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
     }
 
-    public static CardInfo getSecondCardInfo () {
-
-        return new CardInfo("5559000000000002");
+    public static VerificationCode getVerificationCode(AuthInfo authInfo) {
+        return new VerificationCode("12345");
     }
 
+    public static CardValue getFirstCardNumber() {
+        return new CardValue("5559 0000 0000 0001");
+    }
+
+    public static CardValue getSecondCardNumber() {
+        return new CardValue("5559 0000 0000 0002");
+    }
+
+    public static int getTransfer(int balance) {
+        return new Random().nextInt(balance) + 1;
+    }
+
+    public static int getImpossibleTransfer(int balance) {
+        return Math.abs(balance) + new Random().nextInt(balance);
+    }
 }
--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,38 +1,34 @@
 package ru.netology.page;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import ru.netology.data.DataHelper;
+import lombok.val;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DashboardPage {
-    private SelenideElement heading = $("[data-test-id=dashboard]");
-    private ElementsCollection cards = $$(".list__item");
-    private final String balanceStart = "баланс: ";
-    private final String balanceFinish = " р.";
+    private SelenideElement header = $("[data-test-id=dashboard]");
+    private ElementsCollection cards = $$(".list__item div");
+    private String button = "[data-test-id=action-deposit]";
+    private String balanceStart = "баланс:";
+    private String balanceFinish  = " р.";
 
     public DashboardPage() {
-        heading.shouldBe(visible);
+        header.shouldBe(visible);
     }
 
-    public int getCardBalance(DataHelper.CardInfo cardInfo) {
-        var text = cards.findBy(Condition.text(cardInfo.getCardNumber().substring(12, 16))).getText();
-        return extractBalance(text);
-    }
-
-    public MoneyTransferForCard choosCardForTransfer(DataHelper.CardInfo cardInfo) {
-        cards.findBy(Condition.text(cardInfo.getCardNumber().substring(12, 16))).$("[data-test-id=action-deposit]").click();
-        return new MoneyTransferForCard();
-    }
-
-    private int extractBalance(String text) {
-        var start = text.indexOf(balanceStart);
-        var finish = text.indexOf(balanceFinish);
-        var value = text.substring(start + balanceStart.length(), finish);
+    public int extractBalance(String text) {
+        String cardValue = cards.get(Integer.parseInt(text)).text();
+        val start = cardValue.indexOf(balanceStart);
+        val finish = cardValue.lastIndexOf(balanceFinish);
+        val value  = cardValue.substring(start + balanceStart.length(), finish).trim();
         return Integer.parseInt(value);
+    }
+
+    public TransferPage pressReplCard(int index) {
+        SelenideElement buttonReplCard = cards.get(index).find(button);
+        buttonReplCard.click();
+        return new TransferPage();
     }
 }
